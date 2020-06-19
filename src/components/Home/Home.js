a6ebbcc1e8ac16a1b738e6e29bfc9c5e098d06d0
Trend20 <enockomondi305@gmail.com>
2020-06-19 12:36:40 -0400

    app

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
  
 class Home extends Component {
     render() { 
         return (
             <div>
-                Home
+                <Navbar />
             </div>
         );
     }
