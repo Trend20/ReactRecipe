99ee4b22cfccde7f64f0b22aedc4191ac79a8167
Trend20 <enockomondi305@gmail.com>
2020-06-19 13:33:27 -0400

    card

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  recepie1 from '../assets/recipe1.jpeg';
  
 class Home extends Component {
     render() { 
         return (
-            <div>
-                Home Page
+            <div className="container" style={{width:'18rem'}}>
+                <div className="card">
+                    <img className="card-img-top" src={recepie1} />
+                </div>
+                <div className="card-body">
+                    <h1 className="card-title">Recipe1</h1>
+                    <p className="card-text">This is how to prepare Kuon</p>
+                    <a href="#" className="btn btn-primary">Explore</a>
+                </div>
             </div>
         );
     }
