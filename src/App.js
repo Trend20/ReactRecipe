79e5edb4c816efd02ee86a833a5fcd49d129a659
Trend20 <enockomondi305@gmail.com>
2020-06-19 12:50:06 -0400

    audit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Home/Navbar';
 
 function App() {
   return (
     <div className="container-fluid">
-      <Home />
+      {/* <Home /> */}
+      <Navbar />
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() { 
         return (
             <div className="">
-                <Navbar />
+                {/* <Navbar /> */}
             </div>
         );
     }
