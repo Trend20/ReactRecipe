dc08aeb1fd269be7643a9060126ff6b801c2078a
Trend20 <enockomondi305@gmail.com>
2020-06-19 13:39:02 -0400

    container

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,14 +5,32 @@ import  recepie1 from '../assets/recipe1.jpeg';
 class Home extends Component {
     render() { 
         return (
-            <div className="container" style={{width:'18rem'}}>
+            <div className="container-fluid" style={{width:'70vw', 
+            display:'flex',
+            justifyContent:'space-around',}}>
                 <div className="card">
                     <img className="card-img-top" src={recepie1} />
-                </div>
-                <div className="card-body">
+                    <div className="card-body">
                     <h1 className="card-title">Recipe1</h1>
                     <p className="card-text">This is how to prepare Kuon</p>
                     <a href="#" className="btn btn-primary">Explore</a>
+                </div>
+                </div>
+                <div className="card">
+                    <img className="card-img-top" src={recepie1} />
+                    <div className="card-body">
+                    <h1 className="card-title">Recipe1</h1>
+                    <p className="card-text">This is how to prepare Kuon</p>
+                    <a href="#" className="btn btn-primary">Explore</a>
+                </div>
+                </div>
+                <div className="card">
+                    <img className="card-img-top" src={recepie1} />
+                    <div className="card-body">
+                    <h1 className="card-title">Recipe1</h1>
+                    <p className="card-text">This is how to prepare Kuon</p>
+                    <a href="#" className="btn btn-primary">Explore</a>
+                </div>
                 </div>
             </div>
         );
